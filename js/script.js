6ffd8523b0ad08454482c1610b82be24f1d89b6e
vung nguyen <vunnguyen2701@gmail.com>
2022-07-11 21:57:27 +0700

    finish

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-$(document).ready(function(){
-    $(".message__icon-close").click(function(){
-        $(".message__content").hide();
+$(document).ready(function () {
+    $('.message__icon-close').click(function () {
+        $('.message__content').hide();
     })
 
-    
+    $('.question-section__item').click(function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $(this).find('.hide-content').hide(400);
+            $(this).find('.question-section__icon').text('+');
+        } else {
+            $(this).addClass('active');
+            $(this).find('.hide-content').show(400);
+            $(this).siblings().removeClass('active');
+            $(this).siblings().find('.question-section__icon').text('+');
+            $(this).siblings().find('.hide-content').hide(400);
+            $(this).find('.question-section__icon').text('-');
+        }
+    })
 });
