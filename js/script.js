9d786c90f66f92376bd5ce08a757300ca065e37f
vung nguyen <vunnguyen2701@gmail.com>
2022-07-13 21:33:23 +0700

    version 5

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-    $('.question-section__item').click(function () {
+    $('.question-content__item').click(function () {
         if ($(this).hasClass('active')) {
             $(this).removeClass('active');
             $(this).find('.hide-content').hide(400);
-            $(this).find('.question-section__icon').text('+');
+            $(this).find('.question-content__icon').text('+');
         } else {
             $(this).addClass('active');
             $(this).find('.hide-content').show(400);
             $(this).siblings().removeClass('active');
-            $(this).siblings().find('.question-section__icon').text('+');
+            $(this).siblings().find('.question-content__icon').text('+');
             $(this).siblings().find('.hide-content').hide(400);
-            $(this).find('.question-section__icon').text('-');
+            $(this).find('.question-content__icon').text('-');
         }
     })
 });
